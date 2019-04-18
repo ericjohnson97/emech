467ba6a013a4f37901168341aa060f4f9a563ded
ericjohnson97 <eric.emj.johnson@gmail.com>
2019-04-17 20:11:01 -0500

    finished

--- L7_eb/lab7.m
+++ L7_eb/lab7.m
@@ -1,27 +1,66 @@
 %E mech lab 7 
 clc; clear all; close all;
 
-data = xlsread('Data/aft_sparr_eb.xlsx');
+data = xlsread('L7_eb/Data/aft_sparr_eb.xlsx');
 
 force = data(:,3).*21843 - 77.4;
-accel = data(:,2);
 freq = data(:,1);
+% T = (100-5)*4;
+% dt = 1/100;
+tvec = [0:95]'; 
 
 Cmems = 300;
-tipAccel = (data(:,2)-mean(data(:,2)))/( Cmems*9.8);
+tipAccel = ((data(:,2)-mean(data(:,2)))*1000*9.8)/( Cmems);
+
+tipDisp = abs(tipAccel)./freq.^2;
+
 
 figure(1)
 subplot(311);
 plot(freq,tipAccel,'k-');
+title('Aft Sparr Acceleration')
 ylabel('Tip Acceleration, m^2/s');
 grid on
 subplot(312);
 plot(freq,force,'k-');
+title('Aft Sparr Force')
 ylabel('Force [N]');
 grid on
 subplot(313);
-% plot(tvec,tipDisp,'k-');
-% ylabel('Tip Displacement Y, m');
-% xlabel('Time, s');
+plot(tvec,tipDisp,'k-');
+title('Aft Sparr Displacement')
+ylabel('Tip Displacement, m');
+xlabel('Time, s');
 grid on
 
+data2 = xlsread('L7_eb/Data/front_sparr_eb.xlsx');
+
+force2 = data2(:,3).*21843 - 77.4;
+
+freq2 = data2(:,1);
+% T = (100-5)*4;
+% dt = 1/100;
+tvec = [0:95]'; 
+
+Cmems = 300;
+tipAccel2 = ((data2(:,2)-mean(data2(:,2)))*1000*9.8)/( Cmems);
+
+tipDisp2 = abs(tipAccel2)./freq2.^2;
+
+figure(2)
+subplot(311);
+plot(freq2,tipAccel2,'k-');
+title('Front Sparr Acceleration')
+ylabel('Tip Acceleration, m^2/s');
+grid on
+subplot(312);
+plot(freq2,force2,'k-');
+title('Front Sparr Force')
+ylabel('Force [N]');
+grid on
+subplot(313);
+plot(tvec,tipDisp2,'k-');
+title('Front Sparr Displacement')
+ylabel('Tip Displacement, m');
+xlabel('Time, s');
+grid on
--- lab6/lab6_krystal.m
+++ lab6/lab6_krystal.m
@@ -21,7 +21,7 @@ files = ["lab6/pizzo/trial_1.xlsx", "lab6/pizzo/trial_2.xlsx", "lab6/pizzo/trial
 for i=1:length(files)
     data = xlsread(files(i));
 
-    tipAccel = (data(:,3)-mean(data(:,3)))/( Cpizzo*9.8);
+    tipAccel = ((data(:,3)-mean(data(:,3)))*1000)/( Cpizzo*9.8);
     tipVel = zeros(length(tvec),1);
     tipDisp = zeros(length(tvec),1);
 
@@ -48,7 +48,7 @@ for i=1:length(files)
     %magAccel = 1/length(tvec)*abs(dftAccel)*2;               % only magnitude at each frequency
     psdAccel = (1/(fs*length(tvec))) * abs(dftAccel).^2;    % calculate power and scale it
     psdAccel(2:end-1) = 2*psdAccel(2:end-1);                % multiply by two to account for negative frequencies
-    fvec = 0:fs/length(tvec):fs/2;                          % generate properly scaled frequency vector
+    fvec(i,:) = 0:fs/length(tvec):fs/2;                          % generate properly scaled frequency vector
 
     figure(i*2)
     subplot(211)
@@ -57,22 +57,22 @@ for i=1:length(files)
     xlabel('Time, s');
     grid on
     subplot(212);
-    plot(fvec,10*log10(psdAccel))
+    plot(fvec(i, :),10*log10(psdAccel))
     %plot(fvec,magAccel, 'b-');
     grid on
     xlabel('Frequency, Hz')
     ylabel('Power/Frequency (dB/Hz)')
     
-    
-    
-    
+  
 end
+
+
 
 files = ["lab6/mems/trial_1.xlsx", "lab6/mems/trial_2.xlsx", "lab6/mems/trial_3.xlsx", "lab6/mems/trial_4.xlsx", "lab6/mems/trial.xlsx"];
 for i=1:length(files)
     data = xlsread(files(i));
 
-    tipAccel = (data(:,1)-mean(data(:,1)))/( Cmems*9.8);
+    tipAccel = ((data(:,1)-mean(data(:,1)))*1000)/( Cmems*9.8);
     tipVel = zeros(length(tvec),1);
     tipDisp = zeros(length(tvec),1);
 
@@ -117,7 +117,7 @@ for i=1:length(files)
     ylabel('Power/Frequency Y (dB/Hz)')
     
     
-    tipAccel = (data(:,2)-mean(data(:,2)))/( Cmems*9.8);
+    tipAccel = ((data(:,2)-mean(data(:,2)))*1000)/( Cmems*9.8);
     tipVel = zeros(length(tvec),1);
     tipDisp = zeros(length(tvec),1);
 
@@ -160,4 +160,52 @@ for i=1:length(files)
     ylabel('Power/Frequency X (dB/Hz)')
     
 end
+data = xlsread(files(3));
+
+tipAccel = ((data(:,1)-mean(data(:,1)))*1000)/( Cmems*9.8);
+tipVel = zeros(length(tvec),1);
+tipDisp = zeros(length(tvec),1);
+
+tipVel = cumtrapz(tipAccel)*dt;
+tipDisp = cumtrapz(tipVel)*dt;
+
+dftAccel3 = fft(tipAccel(1:3000));                               % calculate FFT
+dftAccel3 = dftAccel3(1:3000/2+1);                % throw away negative frequencies
+psdAccel3 = (1/(fs*3000)) * abs(dftAccel3).^2;    % calculate power and scale it
+psdAccel3(2:end-1) = 2*psdAccel3(2:end-1);                % multiply by two to account for negative frequencies
+fvec3 = 0:fs/3000:fs/2;                          % generate properly scaled frequency vector
+
+figure(31)
+subplot(211)
+plot(tvec(1:3000),tipAccel(1:3000),'k-');
+ylabel('Tip Acceleration, m^2/s');
+xlabel('Time, s');
+grid on
+subplot(212);
+plot(fvec3,10*log10(psdAccel3))
+%plot(fvec,magAccel, 'b-');
+grid on
+xlabel('Frequency, Hz')
+ylabel('Power/Frequency (dB/Hz)')
+
+
+dftAccel1 = fft(tipAccel(3000:4000));                               % calculate FFT
+dftAccel1 = dftAccel1(1:1000/2+1);                % throw away negative frequencies
+psdAccel1 = (1/(fs*1000)) * abs(dftAccel1).^2; % calculate power and scale it
+size(psdAccel1)
+psdAccel1(2:end-1) = 2*psdAccel1(2:end-1);                % multiply by two to account for negative frequencies
+fvec1 = 0:fs/1000:fs/2;                          % generate properly scaled frequency vector
+
+figure(32)
+subplot(211)
+plot(tvec(3000:4000),tipAccel(3000:4000),'k-');
+ylabel('Tip Acceleration, m^2/s');
+xlabel('Time, s');
+grid on
+subplot(212);
+plot(fvec1,10*log10(psdAccel1))
+%plot(fvec,magAccel, 'b-');
+grid on
+xlabel('Frequency, Hz')
+ylabel('Power/Frequency (dB/Hz)')
 
