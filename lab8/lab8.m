d5fe6b7f847aab522b2c2f13b87d3fa7823d5bbe
ericjohnson97 <eric.emj.johnson@gmail.com>
2019-04-24 11:36:58 -0500

    started lab8

--- lab8/lab8.m
+++ lab8/lab8.m
@@ -8,15 +8,19 @@ for i=1:length(sheets)
     data{i}=xlsread(sheets(i)); 
     t{i} = 1/1000:(1/1000):length(data{i})/1000;
     accel{i} = ((data{i}(:,2)-mean(data{i}(:,2)))*1000)/( Cpizzo*9.8);
-
+    angle{i} = ((data{i}(:,3)/5)-.5)*(90/.8);
+    %angle{i} = ((data{i}(:,3)/5)) + (45/.5);
+    %deltaX = sqrt(3^2 + 4.45^2 - 2*3*4.45*cosd(angle))
+    
+    
     figure(i)
     subplot(211);
     plot(t{i},accel{i},'k-');
     ylabel('Acceleration [m^2/s]');
     grid on
     subplot(212);
-    plot(t{i},data{i}(:,3),'k-');
-    ylabel('angle [rad]');
+    plot(t{i},angle{i},'k-');
+    ylabel('angle [degrees]');
     grid on
 
 end
