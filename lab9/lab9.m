f3a5f96150563f8e98a1c64b7aed9539f8fc7bb0
ericjohnson97 <eric.emj.johnson@gmail.com>
2019-04-26 01:17:36 -0500

    started lab9

---
+++ lab9/lab9.m
@@ -0,0 +1,22 @@
+% emech lab 9: Digital Image Correlation 
+clc; clear all; close all;
+
+
+
+aluminum = xlsread('aluminum.xlsx');
+poly = xlsread('poly.xlsx');
+
+figure(1)
+plot(aluminum(:,2), aluminum(:,3))
+hold on 
+title('Force vs Displacement Aluminum')
+xlabel('displacement [mm]')
+ylabel('force [N]')
+
+figure(2)
+plot(poly(:,2), poly(:,3))
+hold on 
+title('Force vs Displacement Polycarbonate')
+xlabel('displacement [mm]')
+ylabel('force [N]')
+
